14342e52716099c34793d17ce0b9d3fc6d9132a1
reina00 <84447083+reina00@users.noreply.github.com>
2025-12-24 01:02:52 +0900

    2-8

--- Study/2-8/Task2_8.java
+++ Study/2-8/Task2_8.java
@@ -20,16 +20,18 @@ public class Task2_8 {
 
 
     // ① 5 と 16 を加算した値を代入する、変数 plus を作成して下さい。
-    int plus = 0;   
-     plus  = 5 + 16;
+    int plus;   
+     plus  = 5 ;
+     plus += 16;
+
 
 
 
 
     // ② 27 から 7 を減算した値を代入する、変数 minus を作成して下さい。
-        int minus = 0;
-        minus = 27 - 7;
-
+        int minus ;
+        minus = 27 ;
+        minus -=7;
 
 
 
@@ -41,9 +43,9 @@ public class Task2_8 {
 
 
     // ④ 20 を 6 で割った余りを代入する、変数 remi を作成して下さい。
-int remi = 0;
-remi = 20 % 6;
-
+int remi;
+remi = 20 ;
+remi %= 6;
 
 
     // ⑤下記の処理について、何をしているのかコメントを記入して下さい。
