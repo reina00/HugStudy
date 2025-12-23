7875c63448da2917d41b7db15c8b6b86703e048f
reina00 <84447083+reina00@users.noreply.github.com>
2025-12-23 21:08:31 +0900

    2-7

--- Study/2-7/Task2_7.java
+++ Study/2-7/Task2_7.java
@@ -23,8 +23,12 @@ public class Task2_7 {
         *   2番目の要素に 「もも」 を代入
         *   3番目の要素に 「ぶどう」 を代入
         */
-        String [] strArray= {"りんご","もも","ぶどう"};
-        
+
+        String [] strArray= new String[3];
+        strArray[0] = "りんご";
+        strArray[1] = "もも";
+        strArray[2] = "ぶどう";
+
 
        // ④ ③で作成した配列の3番目の要素を出力しなさい。
         System.out.println( strArray[2]);
