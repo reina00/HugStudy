cc053df6df917631533f193125bd3554dd795a4f
reina00 <84447083+reina00@users.noreply.github.com>
2026-01-18 22:01:45 +0900

    2-9

--- Study/2-9/Task2_9.java
+++ Study/2-9/Task2_9.java
@@ -45,23 +45,27 @@ public class Task2_9 {
 
     // ① 「name」の値が「USER_NAME」と等しく、「 pass 」の値が「USER_PASSWORD」と等しい場合。
     // 定数を使用して「 ログイン成功です。 」と出力して下さい。
-if (name == USER_NAME && pass == USER_PASSWORD) 
+if (name.equals(USER_NAME) && pass.equals(USER_PASSWORD)){ 
   System.out.println(CONST_MSG_SUCCESS);
+}
     // ② 「USER_NAME」の値のみ等しい場合。
     // 定数を使用して「 パスワードに誤りがあります。 」 と出力して下さい。
-else if (name ==  USER_NAME && pass !=  USER_PASSWORD ) 
+else if (name.equals(USER_NAME)) { 
   System.out.println(CONST_MSG_ERROR_PASS);
+}
 
 
     // ③ 「USER_PASSWORD」の値のみ等しい場合。
     //定数を使用して「 名前に誤りがあります。 」と出力して下さい。
-else if (name !=  USER_NAME &&pass == USER_PASSWORD) 
+else if (pass.equals(USER_PASSWORD)) {
   System.out.println(CONST_MSG_ERROR_NAME);
+}
 
     // ④ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。
     //定数を使用して「 入力情報に誤りがあります。 」と出力して下さい。
-else 
+else {
   System.out.println(CONST_MSG_ERROR_INPUT);
+}
 
   }
 }
